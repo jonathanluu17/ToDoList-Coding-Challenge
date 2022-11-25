494480bd76ef1a7148687b38eb833ab8fd05871e
Jonathan <jonathanluu17@gmail.com>
2022-11-25 12:15:00 -0800

    email validation & password form set up

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,45 @@
 import React, { Component, useEffect, useState } from 'react'
+import './Login.css'
 
 export function LoginPage() {
     // initialize form fields in state
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
-
-
+    const [inputs, setInput] = useState({
+        email: '',
+        password: ''
+    })
 
     // initialize state for updating login input styling
-    const [emailColor, setEmailColor] = useState('black')
+    const [emailStyle, setEmailStyle] = useState({
+        borderColor: '1px solid black',
+        textvisible: 'hidden'
+    })
+
+    // useEffect(() => {
+    //     console.log(inputs)
+    // })
+
+
+    // validate email onchange
+    const emailOnchange = (e) => {
+        // validate email format
+        if (e.target.value && !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e.target.value)){
+            setEmailStyle({
+                borderColor: '1px solid red',
+                textvisible: 'visible'
+            })
+        } else {
+            setEmailStyle({
+                borderColor: '1px solid black',
+                textvisible: 'hidden'
+            })
+            setInput({...inputs, email:e.target.value})
+        }
+    }
+
+    // password onchange
+    const passOnchange = (e) => {
+        setInput({...inputs, password:e.target.value})
+    }
 
     return (
         <div>
@@ -19,15 +50,18 @@ export function LoginPage() {
                 <div className="inputsMainContainer">
                     <div className="emailContainer">
                         <h5>Email</h5>
-                        <div className="emailWrapper" style={{border: '1px dashed red'}}>
-                            
+                        <div className="emailWrapper" style={{border: emailStyle.borderColor}}>
+                            <input className="emailInput" type='text' placeholder='user@rapptrlabs.com' onChange={emailOnchange} />
                         </div>
+                        <div className='textWarning' style={{color: 'red', visibility: emailStyle.textvisible}}>Not a valid email</div>
                     </div>
                     <div className="passContainer">
-
+                        <h5>Password</h5>
+                        <div className="passWrapper" style={{border:'1px solid black'}}>
+                            <input className="passInput" type='password' onChange={passOnchange} />
+                        </div>
                     </div>
                 </div>
-                Test 
             </div>
         </div>
     )
